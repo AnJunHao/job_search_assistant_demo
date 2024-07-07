689a3337221bdce387798d6273cb3327475c6099
AnJunHao <55536952+AnJunHao@users.noreply.github.com>
2024-07-07 14:26:01 +0800

    Modified __init__.py to accomodate new functions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 # Description: This file is used to import all the functions from the .py files in the utils folder.
 from .pdf_handler import pdf_to_string
 from .prompt import welcome_users, comments, keywords, revise
+from .resume_optimizer import process_and_annotate_pdf
